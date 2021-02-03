6046b68680f3cf8507f2e357a3ebcbd825558b6a
Koala <72527434+Camel07@users.noreply.github.com>
2021-02-03 22:50:45 +0800

    Update init.sh

--- init/init.sh
+++ init/init.sh
@@ -18,9 +18,9 @@ trap handleSigInt INT
 initUserge() {
     printLogo
     assertPrerequisites
-    sendMessage "Initializing USERGE-X ..."
+    sendMessage "Initializing KampangUsergay ..."
     assertEnvironment
-    editLastMessage "Starting USERGE-X ..."
+    editLastMessage "Starting KampangUsergay ..."
     printLine
 }
 
@@ -30,7 +30,7 @@ startUserge() {
 }
 
 stopUserge() {
-    sendMessage "Exiting USERGE-X ..."
+    sendMessage "Exiting KampangUsergay ..."
     endLogBotPolling
     exit 0
 }
